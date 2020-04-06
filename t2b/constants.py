407573bfe160c2142d075c95e3d55ab10a6335b3
Xavier Tolza <tolza.xavier@gmail.com>
2020-04-06 22:34:44 +0200

    Ajustements

--- t2b/constants.py
+++ t2b/constants.py
@@ -8,10 +8,10 @@ with open(join(dirname(abspath(__file__)), "correction.txt"), "r") as fp:
     corrections = fp.read().replace("\n", "")
 corrections = np.array(list(corrections)).astype(np.uint8).reshape((-1,) + tuple(Nb_dots))
 corrections = corrections[:, :, ::-1]
+corrections[1] = corrections[1, :, ::-1]
 corrections = np.array([-1, 0, 1, 2, 3, -1, 4, 5, 6, 7])[corrections]
-corrections_bool = corrections[:, :, :, None] == np.unique(corrections.ravel())
 
 is_correct = np.array([
     corrections[0] == 6,
-    np.logical_or(corrections[1] == 3, corrections[1] == 4)
+    np.logical_or(corrections[1] == 2, corrections[1] == 3)
 ])
