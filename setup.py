53af4ae9b077a8efc21e23caf0ec8d27b3552900
Xavier Tolza <xto@ffly4u.com>
2020-04-06 08:03:47 +0200

    Packaging app

--- setup.py
+++ setup.py
@@ -1,43 +1,43 @@
 from setuptools import setup
+import os
+import re
+from multiprocessing import cpu_count
+from os import listdir
+from os.path import join
+from distutils.extension import Extension
+
+DEBUG = os.environ.get("DEBUG")
+DEBUG = DEBUG is not None and DEBUG == "1"
 
 kwargs = {}
 name = 't2b'
 
 try:
-    import os
-    import re
-    from multiprocessing import cpu_count
-    from os import listdir
-    from os.path import join
     from Cython.Build import cythonize
-
-    DEBUG = os.environ.get("DEBUG")
-    DEBUG = DEBUG is not None and DEBUG == "1"
-    # Handle cython files
-    files = listdir(name)
-    r = re.compile(".+\.pyx")
-
-    try:
-        cython_files = [i for i in listdir(name) if r.fullmatch(i) is not None]
-    except AttributeError:
-        cython_files = [i for i in listdir(name) if r.match(i) is not None]
-    if len(cython_files):
-        from distutils.extension import Extension
-
-        extra_compile_args = ['-fopenmp']
-        if DEBUG:
-            extra_compile_args.append("-O0")
-        extensions = [Extension(join(name, i.split(".")[0]).replace("/", "."), [join(name, i)],
-                                language="c++",
-                                extra_compile_args=extra_compile_args,
-                                extra_link_args=['-fopenmp'])
-                      for i in cython_files]
-
-        kwargs.update(dict(
-            ext_modules=cythonize(extensions, annotate=True, gdb_debug=DEBUG, nthreads=cpu_count())
-        ))
 except ImportError:
-    pass
+    raise ImportError("Vous devez installer cython en premier: pip install cython")
+
+# Handle cython files
+files = listdir(name)
+r = re.compile(".+\.pyx")
+
+try:
+    cython_files = [i for i in listdir(name) if r.fullmatch(i) is not None]
+except AttributeError:
+    cython_files = [i for i in listdir(name) if r.match(i) is not None]
+if len(cython_files):
+    extra_compile_args = ['-fopenmp']
+    if DEBUG:
+        extra_compile_args.append("-O0")
+    extensions = [Extension(join(name, i.split(".")[0]).replace("/", "."), [join(name, i)],
+                            language="c++",
+                            extra_compile_args=extra_compile_args,
+                            extra_link_args=['-fopenmp'])
+                  for i in cython_files]
+
+    kwargs.update(dict(
+        ext_modules=cythonize(extensions, annotate=True, gdb_debug=DEBUG, nthreads=cpu_count())
+    ))
 
 setup(
     name=name,
@@ -56,10 +56,9 @@ imutils""".split('\n'),
     entry_points={
         'console_scripts': ['t2b=t2b.cli:cli'],
     },
-    package_data={name: ['t2b/*.txt', 't2b/*.xz', 't2b/*.so', 't2b/*.pyx']},
+    package_data={name: ['t2b/*.txt', 't2b/*.xz']},
     include_package_data=True,
-    data_files=['t2b/correction.txt', 't2b/motifs.tar.xz', 't2b/c_funs.cpython-37m-x86_64-linux-gnu.so',
-                't2b/c_funs.pyx'],
+    data_files=['t2b/correction.txt', 't2b/motifs.tar.xz'],
     **kwargs
 )
 
