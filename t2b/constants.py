02d499998726811c443430d229a8301bbeedfbdf
Xavier <xto@ffly4u.com>
2020-04-09 18:43:24 +0200

    bugfix

--- t2b/constants.py
+++ t2b/constants.py
@@ -7,8 +7,6 @@ Nb_dots = np.array([40, 25])  # nombre de points
 with open(join(dirname(abspath(__file__)), "correction.txt"), "r") as fp:
     corrections = fp.read().replace("\n", "")
 corrections = np.array(list(corrections)).astype(np.uint8).reshape((-1,) + tuple(Nb_dots))
-corrections = corrections[:, :, ::-1]
-# corrections[1] = corrections[1, :, ::-1]
 corrections = np.array([-1, 0, 1, 2, 3, -1, 4, 5, 6, 7])[corrections]
 
 is_correct = np.array([
